4fd61f890b50cd0efa65a08e60606e2d9ed7bd24
Choi Seokjun <Choi.Seokjun@outlook.com>
2020-10-28 15:03:08 +0900

    data gen

---
+++ spaBasisCNN.py
@@ -0,0 +1,75 @@
+#STD
+import math
+
+#scipy family
+import numpy as np
+import scipy.stats as stats
+from scipy.spatial.distance import cdist
+from scipy.linalg import cholesky
+import matplotlib.pyplot as plt
+
+# #FOR ML
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras import layers
+
+def test_data_generator(n_knot=100, n_data=1000, n_cv=500, seed_val=12345):
+    # set the places
+    randGenerator = np.random.default_rng(seed=seed_val)
+    gridLoc_knot = np.array([(x, y) for x in range(math.floor(n_knot**0.5)) for y in range(math.floor(n_knot**0.5))], dtype=float)/math.floor(n_knot**0.5)
+    gridLoc_data = randGenerator.random((n_data,2))
+    gridLoc_cv = randGenerator.random((n_cv,2))
+    gridLoc_complete = np.concatenate((gridLoc_knot, gridLoc_data, gridLoc_cv), axis=0)
+
+    # spatial covariate
+    Xmat_knot = randGenerator.random((n_knot,2))  #2 dim covariate
+    Xmat_data = randGenerator.random((n_data,2))
+    Xmat_cv = randGenerator.random((n_cv,2))
+    Xmat_complete = np.concatenate((Xmat_knot, Xmat_data, Xmat_cv), axis=0)
+    # print(Xmat_complete.shape) #(1600,2)
+
+    # distance matrix
+    distMat_complete = cdist(gridLoc_complete, gridLoc_complete,'euclidean')
+    # print(distMat_complete.shape) #(1600,1600)
+
+    # Generate Data following model
+    MaternCov_params = {"phi" : 0.2, "sigma2" : 1, "beta" : np.array([2,2], dtype=float)}
+    MaternCov_mat = (1 + (5**0.5 * distMat_complete / MaternCov_params["phi"]) +(5 * distMat_complete**2) / (3*MaternCov_params["phi"]**2)) * \
+        np.exp(-(5**0.5 *(distMat_complete/MaternCov_params["phi"])))
+    # print(MaternCov_mat.shape) #(1600,1600)
+    MaternCov_mat *= MaternCov_params["sigma2"]
+    MaternCov_TSseed = randGenerator.normal(loc=0.0, scale=1.0, size=n_knot + n_data + n_cv)
+    Wmat = np.matmul(cholesky(MaternCov_mat).T, MaternCov_TSseed)
+    # print(Wmat.shape) #(1600,)
+    # print(np.max(Wmat), np.min(Wmat))
+    Zmat_intensity = np.exp(Wmat + np.matmul(Xmat_complete, MaternCov_params['beta']))
+    Zmat = randGenerator.poisson(lam=Zmat_intensity)
+    # print(Zmat.size) #1600
+
+    # thin plate splinte basis(below:TPS)
+    # knot data CV
+    ind_knot = slice(n_knot)
+    ind_data = slice(n_knot, n_data+n_knot)
+    ind_cv = slice(n_knot+n_data, n_knot+n_data+n_cv)
+    distMat_data_knot = distMat_complete[ind_data, ind_knot]
+    distMat_cv_knot = distMat_complete[ind_cv, ind_knot]
+    TPSmat_data = (distMat_data_knot**2) * (np.log(distMat_data_knot))
+    TPSmat_cv = (distMat_cv_knot**2) * (np.log(distMat_cv_knot))
+    # print(TPSmat_data.shape) #1000, 100
+    # print(TPSmat_cv.shape) #500, 100
+
+    # design matrix
+    designMat_data = np.c_[Xmat_data, TPSmat_data] #여기에 1을 넣어버리면 인생이 좀 더 편해질듯
+    designMat_cv = np.c_[Xmat_cv, TPSmat_cv]
+    # print(designMat_data.shape) #1000, 102
+    # print(designMat_cv.shape) #500, 102
+    
+    # response matrix
+    Zmat_data = Zmat[ind_data]
+    Zmat_cv = Zmat[ind_cv]
+    return (designMat_data, Zmat_data, gridLoc_data, designMat_cv, Zmat_cv, gridLoc_cv)
+
+
+
+# EDA: figures
+(designMat_data, Zmat_data, gridLoc_data, designMat_cv, Zmat_cv, gridLoc_cv) = test_data_generator()
